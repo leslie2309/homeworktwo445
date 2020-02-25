53ecc903686b915fb6612b75d4858de6f2c86510
Leslie <santanaleslie18@gmail.com>
2020-02-24 23:46:38 -0800

    commit on 'master'

--- contact/main.js
+++ contact/main.js
@@ -1,17 +1,17 @@
-var iputName = document. getElementById('input-name');
+var inputName = document. getElementById('input-name');
 var inputMail = document.getElementById('input-mail');
 var inputMessage = document.getElementById('input-message');
 var button = document.querySelector('.submit-button');
 var regEx = /\S+@\S+\.\S+/;
-var form = document.querySelector('.submit-button');
+var form = document.querySelector('.form');
 
 function checkInput() {
-    if (inputName.value.trim() !== "" && regEx.test (inputMail.value) && inoutMessage.value.trim() !==""){
+    if (inputName.value.trim() !== "" && regEx.test (inputMail.value) && inputMessage.value.trim() !== "") {
         button.style.backgroundColor = '#fa923f'; 
         button.disabled = false; 
     } else {
         button.style.backgroundColor= '#979695'
-        button.disabled =true;
+        button.disabled = true;
     }
 }
 
